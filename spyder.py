5df48f7e08c11c5b801b65d622aa5a36300668f1
user1_1_11 <user1_1_11@yahoo.com>
2013-12-15 00:09:48 -0800

    Made DB files excluded

--- spyder.py
+++ spyder.py
@@ -6,7 +6,8 @@ import urllib2
 from lepl.apps.rfc3696 import HttpUrl
 #For time stamp
 import time
-
+#SQL thingy
+import sqlite3
 def timestamp():
     #gets time since epoch
     now = time.localtime(time.time())
@@ -14,6 +15,7 @@ def timestamp():
     localtime = time.strftime("%H:%M:%S", now)
     #makes the final time stamp format
     return "["+ localtime +"] "
+
 #URL is the URL to be checked
 def validator(url):
     #checks for valid URL
@@ -29,40 +31,67 @@ def union(p,q):
             p.append(e)
 
 def spyder(SeedUrl, target):
+    #initalize db connection
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    #creates toCrawl database 0.o
+    c.execute("""CREATE TABLE toCrawl();""")
+    #creates crawled database 0.o
+    c.execute("""CREATE TABLE crawled();""")
+    #get start of process
+    startTime = time.localtime()
+    seqNum = 0
     #DB of links to crawl in the future
+    #tocrawTable = "toCrawl.db"
     tocrawl=[SeedUrl]
     #DB of links that have been crawled
     crawled=[]
     #status message at start of crawl seed url is the first
     print timestamp() + "STARTING CRAWL WITH SEED: " + tocrawl[0]
     while tocrawl:
-        page=tocrawl.pop(0)
-        if validator(page) and page not in crawled:
+        page="SELECT TOP %s url FROM toCrawl;", pos
+        #Ghetto fixes for common errors...
+        if validator(page) and page not in crawled and page.find(".pdf") == -1:
+            #Uncomment the line below for Url Debugging useful for finding which website is at fault for the errors you are having
             #print  timestamp() + page
             try:
                 pagesource=urllib2.urlopen(page)
             except urllib2.HTTPError, e: #handler for HTTP exception and errors
                 #Error message: Time stamp, Error code, page that caused the error 
-                print timestamp() + "ERROR: " + str(e.code) + " " + page
+                print timestamp() + "ERROR: " + str(e.code)
+                crawled.append(page)
             except urllib2.URLError, e: #handler for URL loading errors
                 #Error message: Time Stamp, Error code, page that caused the error
-                print timestamp() + "ERROR: " + str(e.code) + " " + page
+                print timestamp() + "ERROR: " + str(e)
+                #fixes problem with loading error pages being reopened
+                crawled.append(page)
             else:
+                Extension = "/"
+                domain = ''
+                urldomain0 = page.split('/')
+                domain = urldomain0.pop(2)
                 #loads the page and reads it
-                s=pagesource.read()
+                s = pagesource.read()
                 soup=BeautifulSoup.BeautifulSoup(s)
                 #finds all links
                 links=soup.findAll('a',href=True)
                 if target.find("http://")== -1:
-                    content = soup.findall('')
-                #takes every link and takes the URL
+                    pass
+                    #content = soup.findall('')
+                #takes every link and takes the URL to scrape
                 for index, item in enumerate(links):
                     links[index] = item['href']
                 #checks if page is crawled and adds links if not in crawled... This is somewhat depreciated
                 union(tocrawl,links)
                 crawled.append(page)
+                seqNum = seqNum + 1
+                #print info to sqldatabase
+                c.execute('')
                 #debug message when page is finished being crawled
-                print timestamp() + "Finished Crawl:: " + str(len(tocrawl)) + " left to crawl " 
-    return crawled
+                print timestamp() + "Finished Crawl:: " + str(len(tocrawl)) + " left to crawl " + " Crawled " + str(seqNum) + " pages " + domain
+                #output for seq number
+    endTime = time.localtime()
 
-spyder('http://google.com','http://facebook.com')
+    return crawled, endTime-startTime
+
+spyder('http://google.com','hi')
